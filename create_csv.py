bbec728e8893a265df116022a339305018d8b623
Igor Ivanov <31894518+IVIGOR13@users.noreply.github.com>
2018-11-21 09:15:46 +0300

    Update create_csv.py

--- create_csv.py
+++ create_csv.py
@@ -1,41 +1,45 @@
+#
+#   Images are read from all directories in the folder in which the script resides.
+#   The directory name is an image label.
+#
 import os
 import glob
 import pandas as pd
-from PIL import Image 
+from PIL import Image
 
-def xml_to_csv(directorys):
-    dir_in = directorys[1]
-    for directory in dir_in:
-        xml_list = []
-        for image_file in glob.glob(directory + '/*.png'):
+def img_to_csv(dirs_in):
+    xml_list = []
+    for dir_in in dirs_in:
+        for image_file in glob.glob(dir_in + '/*.png'):
             image = Image.open(image_file)
             width = image.size[0]
             height = image.size[1]
 
-            a = directory.split('/')
+            a = dir_in.split('\\')
             label = a[len(a)-1]
 
-            b = image_file.split('/')
+            b = image_file.split('\\')
             filename = b[len(b)-1]
 
-            value = (filename[:len(filename)-4],
-                width,
-                height,
-                label,
-                0,
-                0,
-                width,
-                height
-                )
+            value = (filename,
+                     width,
+                     height,
+                     label,
+                     0,
+                     0,
+                     width,
+                     height
+                     )
             xml_list.append(value)
-
     column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
     return xml_df
 
+
 def main():
-    directorys = [x[0] for x in os.walk(os.path.join(os.getcwd()))]
-    xml_df = xml_to_csv(directorys)
+    dirs = [x[0] for x in os.walk(os.path.join(os.getcwd()))]
+    dirs_in = dirs[1:]
+    xml_df = img_to_csv(dirs_in)
     xml_df.to_csv('data_labels.csv', index=None)
     print('Successfully create csv file.')
 
