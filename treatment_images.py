ef673a54ab3bc1cb36007a29e92a2446504475b6
Igor Ivanov <31894518+IVIGOR13@users.noreply.github.com>
2018-11-25 16:47:57 +0300

    Update treatment_images.py

--- treatment_images.py
+++ treatment_images.py
@@ -1,49 +1,141 @@
 import random
 import os
 import cv2
-import math
 import numpy as np
-from PIL import Image, ImageDraw, ImageFilter
+from PIL import Image, ImageDraw
+
 
 class TreatmentImages:
-    def __init__(self, dir, name, treat):
+    def __init__(self, dir, name, treat, index_transform):
         self.directory_in = dir
         self.name_in = name[:len(name)-4]
-
-        self.image = Image.open(self.directory_in + '/' + name)
+        self.image = Image.open(self.directory_in + '\\' + name)
         self.width = self.image.size[0]
         self.height = self.image.size[1]
 
-        self.directory_out = dir + '/out/'
+        self.directory_out = dir + '\\out\\'
 
         if not os.path.exists(self.directory_out):
             os.makedirs(self.directory_out)
 
-        for i in range(len(treat)):
-            print('0')
-            if treat[i][0] == 1:
-                self.transform_black_white(treat[i][1])
-            if treat[i][0] == 2:
-                self.transform_noises(treat[i][1])
-            if treat[i][0] == 3:
-                self.transform_turn(treat[i][1])
-            if treat[i][0] == 4:
-                self.transform_gray_shades()
-            if treat[i][0] == 5:
-                self.transform_strip(treat[i][1])
-            if treat[i][0] == 6:
-                self.transform_glare(treat[i][1])
-            if treat[i][0] == 7:
-                self.transform_blur()
-            if treat[i][0] == 8:
-                self.transform_compress_vertical(treat[i][1])
-            if treat[i][0] == 9:
-                self.transform_stretch_vertical(treat[i][1])
+        if index_transform == 0:
+            for i in range(len(treat)):
+                if treat[i][0] == 1:
+                    img = self.transform_black_white(treat[i][1], self.image.copy())
+                    path = self.directory_out + self.name_in + '_black-white'
+                    img.save((path + '.png'), "PNG")
 
-    def transform_black_white(self, factor):
-        newIm = self.image.copy()
-        pix = newIm.load()
-        draw = ImageDraw.Draw(newIm)
+                if treat[i][0] == 2:
+                    img = self.transform_noises(treat[i][1], self.image.copy())
+                    path = self.directory_out + self.name_in + '_noises'
+                    img.save((path + '.png'), "PNG")
+
+                if treat[i][0] == 3:
+                    img = self.transform_turn(treat[i][1], self.directory_in + '/' + name)
+                    cv2.imwrite(self.directory_out + self.name_in + '_turn_plus' + ".png", img[0])
+                    cv2.imwrite(self.directory_out + self.name_in + '_turn_minus' + ".png", img[1])
+
+                if treat[i][0] == 4:
+                    img = self.transform_gray_shades(self.image.copy())
+                    path = self.directory_out + self.name_in + '_gray_shades'
+                    img.save((path + '.png'), "PNG")
+
+                if treat[i][0] == 5:
+                    img = self.transform_strip(treat[i][1], self.image.copy())
+                    path = self.directory_out + self.name_in + '_strip'
+                    img.save((path + '.png'), "PNG")
+
+                if treat[i][0] == 6:
+                    img = self.transform_glare(treat[i][1], self.image.copy())
+                    path = self.directory_out + self.name_in + '_glare'
+                    img.save((path + '.png'), "PNG")
+
+                if treat[i][0] == 7:
+                    img = self.transform_blur(self.directory_in + '/' + name)
+                    cv2.imwrite(self.directory_out + self.name_in + '_blur' + ".png", img)
+
+                if treat[i][0] == 8:
+                    img = self.transform_compress(treat[i][1], self.image.copy())
+                    path = self.directory_out + self.name_in + '_compress'
+                    img.save((path + '.png'), "PNG")
+
+                if treat[i][0] == 9:
+                    img = self.transform_stretch(treat[i][1], self.image.copy())
+                    path = self.directory_out + self.name_in + '_stretch'
+                    img.save((path + '.png'), "PNG")
+
+
+        if index_transform == 1:
+            path_combo = self.name_combo(self.directory_out + self.name_in + '_combo.png')
+            image_combo = self.image.copy()
+            image_combo.save(path_combo, "PNG")
+
+            for i in range(len(treat)):
+
+                if treat[i][0] == 1:
+                    img = self.transform_black_white(treat[i][1], image_combo)
+                    img.save(path_combo, "PNG")
+
+                if treat[i][0] == 2:
+                    img = self.transform_noises(treat[i][1], image_combo)
+                    img.save(path_combo, "PNG")
+
+                if treat[i][0] == 3:
+                    img = self.transform_turn(treat[i][1], path_combo)
+                    cv2.imwrite(path_combo, img[0])
+
+                if treat[i][0] == 4:
+                    img = self.transform_gray_shades(image_combo)
+                    img.save(path_combo, "PNG")
+
+                if treat[i][0] == 5:
+                    img = self.transform_strip(treat[i][1], image_combo)
+                    img.save(path_combo, "PNG")
+
+                if treat[i][0] == 6:
+                    img = self.transform_glare(treat[i][1], image_combo)
+                    img.save(path_combo, "PNG")
+
+                if treat[i][0] == 7:
+                    img = self.transform_blur(path_combo)
+                    cv2.imwrite(path_combo, img)
+
+                if treat[i][0] == 8:
+                    img = self.transform_compress(treat[i][1], image_combo)
+                    img.save(path_combo, "PNG")
+
+                if treat[i][0] == 9:
+                    img = self.transform_stretch(treat[i][1], image_combo)
+                    img.save(path_combo, "PNG")
+
+                image_combo = Image.open(path_combo)
+            image_combo.save(path_combo, "PNG")
+
+    def name_combo(self, path):
+        expansion = path[len(path)-4:]
+        path = path[:len(path)-4]
+        p = path.split('\\')
+        path = p[:len(p)-1]
+        path = '\\'.join(path) + "\\"
+        name = p[len(p)-1]
+
+        files = os.listdir(path)
+        images = [x for x in files if x.endswith(expansion)]
+        x = 0
+        for i in images:
+            if i.startswith(name):
+                x += 1
+        if not x == 0:
+            return path + name + '_' + str(x) + expansion
+        else:
+            return path + name + expansion
+
+
+
+
+    def transform_black_white(self, factor, img):
+        pix = img.load()
+        draw = ImageDraw.Draw(img)
 
         for i in range(self.width):
             for j in range(self.height):
@@ -54,13 +146,11 @@ class TreatmentImages:
                     a, b, c = 0, 0, 0
                 draw.point((i, j), (a, b, c))
 
-        newIm.save((self.directory_out + self.name_in + '_black-white' + '.jpg'), "JPEG")
+        return img
 
-    def transform_noises(self, factor):
-        newIm = self.image.copy()
-        pix = newIm.load()
-        draw = ImageDraw.Draw(newIm)
-
+    def transform_noises(self, factor, img):
+        pix = img.load()
+        draw = ImageDraw.Draw(img)
         for i in range(self.width):
             for j in range(self.height):
                 rand = random.randint(-factor, factor)
@@ -75,9 +165,9 @@ class TreatmentImages:
                 if (c > 255): c = 255
                 draw.point((i, j), (a, b, c))
 
-        newIm.save((self.directory_out + self.name_in + '_noises' + '.jpg'), "JPEG")
+        return img
 
-    def transform_turn(self, factor):
+    def transform_turn(self, factor, path):
         def turn_image(image, angle):
             image_size = (image.shape[1], image.shape[0])
             image_center = tuple(np.array(image_size) / 2)
@@ -131,150 +221,136 @@ class TreatmentImages:
 
             return result
 
-        image = cv2.imread(self.directory_in + '/' + self.name_in + '.jpg')
-        image_turned = turn_image(image, factor)
-        cv2.imwrite(self.directory_out + self.name_in + '_turn10' + ".jpg", image_turned)
+        image = cv2.imread(path)
+        image_turned1 = turn_image(image, factor)
 
-        image = cv2.imread(self.directory_in + '/' + self.name_in + '.jpg')
-        image_turned = turn_image(image, -factor)
-        cv2.imwrite(self.directory_out + self.name_in + '_turn-10' + ".jpg", image_turned)
+        image = cv2.imread(self.directory_in + '/' + self.name_in + '.png')
+        image_turned2 = turn_image(image, -factor)
+        return [image_turned1, image_turned2]
 
-    def transform_gray_shades(self):
-        newIm = self.image.copy()
-        pix = newIm.load()
-        draw = ImageDraw.Draw(newIm)
+    def transform_gray_shades(self, img):
+        pix = img.load()
+        draw = ImageDraw.Draw(img)
 
         for i in range(self.width):
             for j in range(self.height):
                 S = (pix[i, j][0] + pix[i, j][1] + pix[i, j][2]) // 3
                 draw.point((i, j), (S, S, S))
 
-        newIm.save((self.directory_out + self.name_in + '_gray-shades' + '.jpg'), "JPEG")
+        return img
 
-    def transform_strip(self, factor):
-        for iter in range(5):
-            newIm = self.image.copy()
-            pix = newIm.load()
-            draw = ImageDraw.Draw(newIm)
+    def transform_strip(self, factor, img):
+        pix = img.load()
+        draw = ImageDraw.Draw(img)
 
-            rand = random.randint(0, 100)
-            tin = round(self.height * rand / 100)
-            tout = round(self.height * random.randint(0, 100) / 100)
+        rand = random.randint(0, 100)
+        tin = round(self.height * rand / 100)
+        tout = round(self.height * random.randint(0, 100) / 100)
 
-            if factor > 0:
-                for i in range(self.width):
-                    for j in range(self.height):
-                        ti = round(tin)
-                        if j>ti and j<ti+self.height/8:
-                            if (pix[i, j][0] > 215):
-                                a = 255
-                            else:
-                                a = pix[i, j][0] + 40
-                            if (pix[i, j][1] > 215):
-                                b = 255
-                            else:
-                                b = pix[i, j][1] + 40
-                            if (pix[i, j][2] > 215):
-                                c = 255
-                            else:
-                                c = pix[i, j][2] + 40
+        if factor > 0:
+            for i in range(self.width):
+                for j in range(self.height):
+                    ti = round(tin)
+                    if j>ti and j<ti+self.height/8:
+                        if (pix[i, j][0] > 215):
+                            a = 255
+                        else:
+                            a = pix[i, j][0] + 40
+                        if (pix[i, j][1] > 215):
+                            b = 255
+                        else:
+                            b = pix[i, j][1] + 40
+                        if (pix[i, j][2] > 215):
+                            c = 255
+                        else:
+                            c = pix[i, j][2] + 40
 
-                            draw.point((i, j), (a, b ,c))
+                        draw.point((i, j), (a, b ,c))
 
-                    if tin>tout:
-                        tin = tin + (abs(self.height*rand / 100 - tout) / self.width)
-                    else:
-                        tin = tin - (abs(self.height * rand / 100 - tout) / self.width)
+                if tin>tout:
+                    tin = tin + (abs(self.height*rand / 100 - tout) / self.width)
+                else:
+                    tin = tin - (abs(self.height * rand / 100 - tout) / self.width)
 
-                newIm.save((self.directory_out + self.name_in + '_light_strip_' + str(iter) + '.jpg'), "JPEG")
+        else:
+            for i in range(self.width):
+                for j in range(self.height):
+                    ti = round(tin)
+                    if j > ti and j < ti + self.height / 8:
+                        if (pix[i, j][0] < -factor):
+                            a = 0
+                        else:
+                            a = pix[i, j][0] + factor
+                        if (pix[i, j][1] < -factor):
+                            b = 0
+                        else:
+                            b = pix[i, j][1] + factor
+                        if (pix[i, j][2] < -factor):
+                            c = 0
+                        else:
+                            c = pix[i, j][2] + factor
+                        draw.point((i, j), (a, b, c))
+                if tin > tout:
+                    tin = tin + (abs(self.height * rand / 100 - tout) / self.width)
+                else:
+                    tin = tin - (abs(self.height * rand / 100 - tout) / self.width)
 
-            else:
-                for i in range(self.width):
-                    for j in range(self.height):
-                        ti = round(tin)
-                        if j > ti and j < ti + self.height / 8:
-                            if (pix[i, j][0] < -factor):
-                                a = 0
-                            else:
-                                a = pix[i, j][0] + factor
-                            if (pix[i, j][1] < -factor):
-                                b = 0
-                            else:
-                                b = pix[i, j][1] + factor
-                            if (pix[i, j][2] < -factor):
-                                c = 0
-                            else:
-                                c = pix[i, j][2] + factor
-                            draw.point((i, j), (a, b, c))
-                    if tin > tout:
-                        tin = tin + (abs(self.height * rand / 100 - tout) / self.width)
-                    else:
-                        tin = tin - (abs(self.height * rand / 100 - tout) / self.width)
+        return img
 
-                newIm.save((self.directory_out + self.name_in + '_dark_strip_' + str(iter) + '.jpg'), "JPEG")
+    def transform_glare(self, factor, img):
+        pix = img.load()
+        draw = ImageDraw.Draw(img)
 
-    def transform_glare(self, factor):
-        for iter in range(5):
-            newIm = self.image.copy()
-            pix = newIm.load()
-            draw = ImageDraw.Draw(newIm)
+        radius = self.height // 6
+        Ox = random.randint(0, 100) / 100 * self.width
+        Oy = random.randint(0, 100) / 100 * self.height
 
-            radius = self.height // 6
-            Ox = random.randint(0, 100) / 100 * self.width
-            Oy = random.randint(0, 100) / 100 * self.height
+        if factor > 0:
+            for i in range(self.width):
+                for j in range(self.height):
+                    if (i - Ox) ** 2 + (j - Oy) ** 2 <= radius ** 2:
+                        if (pix[i, j][0] > (255 - factor)):
+                            a = 255
+                        else:
+                            a = pix[i, j][0] + factor
+                        if (pix[i, j][1] > (255 - factor)):
+                            b = 255
+                        else:
+                            b = pix[i, j][1] + factor
+                        if (pix[i, j][2] > (255 - factor)):
+                            c = 255
+                        else:
+                            c = pix[i, j][2] + factor
 
-            if factor > 0:
-                for i in range(self.width):
-                    for j in range(self.height):
-                        if (i - Ox) ** 2 + (j - Oy) ** 2 <= radius ** 2:
-                            if (pix[i, j][0] > (255 - factor)):
-                                a = 255
-                            else:
-                                a = pix[i, j][0] + factor
-                            if (pix[i, j][1] > (255 - factor)):
-                                b = 255
-                            else:
-                                b = pix[i, j][1] + factor
-                            if (pix[i, j][2] > (255 - factor)):
-                                c = 255
-                            else:
-                                c = pix[i, j][2] + factor
+                        draw.point((i, j), (a, b, c))
+        else:
+            for i in range(self.width):
+                for j in range(self.height):
+                    if (i - Ox) ** 2 + (j - Oy) ** 2 <= radius ** 2:
+                        if (pix[i, j][0] > (0 - factor)):
+                            a = 0
+                        else:
+                            a = pix[i, j][0] + factor
+                        if (pix[i, j][1] > (0 - factor)):
+                            b = 0
+                        else:
+                            b = pix[i, j][1] + factor
+                        if (pix[i, j][2] > (0 - factor)):
+                            c = 0
+                        else:
+                            c = pix[i, j][2] + factor
 
-                            draw.point((i, j), (a, b, c))
-            else:
-                for i in range(self.width):
-                    for j in range(self.height):
-                        if (i - Ox) ** 2 + (j - Oy) ** 2 <= radius ** 2:
-                            if (pix[i, j][0] > (0 - factor)):
-                                a = 0
-                            else:
-                                a = pix[i, j][0] + factor
-                            if (pix[i, j][1] > (0 - factor)):
-                                b = 0
-                            else:
-                                b = pix[i, j][1] + factor
-                            if (pix[i, j][2] > (0 - factor)):
-                                c = 0
-                            else:
-                                c = pix[i, j][2] + factor
+                        draw.point((i, j), (a, b, c))
 
-                            draw.point((i, j), (a, b, c))
+        return img
 
-            newIm.save((self.directory_out + self.name_in + '_glare_' + str(iter) + '.jpg'), "JPEG")
+    def transform_blur(self, path):
+        return  cv2.GaussianBlur (cv2.imread(path), (5,5), 0)
 
-    def transform_blur(self):
-        newIm = self.image.copy().filter(ImageFilter.BLUR)
-        newIm.save((self.directory_out + self.name_in + '_blur' + '.jpg'), "JPEG")
+    def transform_compress(self, factor, img):
+        img = img.resize((round(factor*self.width), self.height))
+        return img
 
-    def transform_compress_vertical(self, factor):
-        newIm = self.image.copy()
-        stretch_image = newIm.resize((round(factor*self.width), self.height))
-        stretch_image.save((self.directory_out + self.name_in + '_compress' + '.jpg'), "JPEG")
-
-    def transform_stretch_vertical(self, factor):
-        newIm = self.image.copy()
-        stretch_image = newIm.resize((self.width, round(factor*self.height)))
-        stretch_image.save((self.directory_out + self.name_in + '_stretch' + '.jpg'), "JPEG")
-
-
-
+    def transform_stretch(self, factor, img):
+        img = img.resize((self.width, round(factor*self.height)))
+        return img
